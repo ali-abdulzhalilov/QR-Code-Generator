4fa45d8297e0c0686961b27c6ae66df9ba876b48
marl-solo <mark.an.impostor@gmail.com>
2019-06-29 14:31:07 +0300

    some basic tdd

---
+++ app.py
@@ -0,0 +1,15 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+    abort, render_template, flash, jsonify
+    
+DATABASE = 'app.db'
+DEBUG = True
+SECRET_KEY = 'very_secret'
+USERNAME = 'admin'
+PASSWORD = 'admin'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+if __name__ == '__main__':
+    app.run()
