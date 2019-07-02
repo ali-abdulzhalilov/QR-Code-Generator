27f49c948396ce2881e3f4773257eacc79a43f0a
mark-solo <mark.an.impostor@gmail.com>
2019-07-02 08:51:27 +0300

    database setup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
     abort, render_template, flash, jsonify
     
-DATABASE = 'app.db'
+DATABASE = 'qrc.db'
 DEBUG = True
 SECRET_KEY = 'very_secret'
 USERNAME = 'admin'
@@ -12,4 +12,29 @@ app = Flask(__name__)
 app.config.from_object(__name__)
 
 if __name__ == '__main__':
+    init_db()
     app.run()
+    
+    
+# db utils functions
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+    
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+def get_db():
+    if not hasattr(g, 'sqlite_db'):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'sqlite_db'):
+        g.sqlite_db.close()
